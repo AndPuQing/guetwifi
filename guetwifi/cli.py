d32fd15dbd26a2d5ea2ffc33c98b7bd776b7e027
PuQing <me@puqing.work>
2024-04-11 18:17:30 +0800

    Refactor CLI functionality and fix log file path

--- guetwifi/cli.py
+++ guetwifi/cli.py
@@ -1,57 +1,8 @@
-#!/usr/bin/env python
-# coding:utf-8
-
-import subprocess
-import psutil
 import click
-import os
-import json
 import importlib.metadata as metadata
+from guetwifi.guetwifirunner import NetWork
 
 __version__ = metadata.version("guetwifi")
-_path = os.path.dirname(os.path.abspath(__file__))
-_runner = "guetwifirunner"
-
-
-def _get_pid():
-    runner_pids = []
-    for proc in psutil.process_iter():
-        try:
-            pinfo = proc.as_dict(attrs=["pid", "cmdline"])
-            cmdline = "".join(pinfo["cmdline"])
-            if _runner in cmdline:
-                runner_pids.append(pinfo["pid"])
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
-    return runner_pids
-
-
-def _start():
-    runner_pids = _get_pid()
-    if runner_pids:
-        click.echo("guetwifi is already running")
-        return
-    else:
-        cmd = "python -m guetwifi.{}".format(_runner)
-        subprocess.Popen(cmd, shell=True)
-        runner_pids = _get_pid()
-        if runner_pids:
-            click.echo("guetwifi started")
-        else:
-            click.echo("guetwifi start failed")
-
-
-def _stop():
-    runner_pids = _get_pid()
-    if not runner_pids:
-        click.echo("guetwifi is not running")
-        return
-    else:
-        for pid in runner_pids:
-            p = psutil.Process(pid)
-            p.terminate()
-            click.echo("terminate pid: {}".format(pid))
-        click.echo("guetwifi stopped")
 
 
 @click.group()
@@ -66,63 +17,6 @@ def guetwifi(ctx, debug):
 
 
 @guetwifi.command()
-@click.pass_context
-def start(ctx):
-    """Start guetwifi"""
-    # TODO: enable debug mode
-    click.echo("Start guetwifi...")
-    _start()
-
-
-@guetwifi.command()
-def stop():
-    """Stop guetwifi"""
-    click.echo("Stop guetwifi...")
-    _stop()
-
-
-@guetwifi.command()
-def restart():
-    """Restart guetwifi"""
-    click.echo("Restart guetwifi...")
-    stop()
-    start()
-
-
-@guetwifi.command()
-def version():
-    """Show guetwifi version"""
-    click.echo("guetwifi version: {}".format(__version__))
-
-
-@guetwifi.command()
-def status():
-    """Check guetwifi status"""
-    runner_pids = _get_pid()
-    if runner_pids:
-        click.echo("guetwifi is running")
-        click.echo("pid: {}".format(runner_pids))
-    else:
-        click.echo("guetwifi is not running")
-
-
-@guetwifi.command()
-@click.option("-n", "--number", default=10, help="The number of log")
-def log(number: int):
-    """Show guetwifi log"""
-    log_path = os.path.join(_path, "network.log")
-    if os.path.exists(log_path):
-        with open(log_path, "r") as f:
-            lines = f.readlines()
-            if len(lines) > number:
-                lines = lines[-number:]
-            for line in lines:
-                click.echo(line, nl=False)
-    else:
-        click.echo("No log file found")
-
-
-@guetwifi.command()
 @click.option("-a", "--account", default=None, help="Your GUET-WIFI account", type=int)
 @click.option("-p", "--password", default=None, help="Your GUET-WIFI password")
 @click.option(
@@ -132,16 +26,13 @@ def log(number: int):
     type=click.Choice(["cmcc", "unicom", "telecom", ""], case_sensitive=False),
     help="Your want to login isp",
 )
-def config(account: int, password: str, isp: str):
+def connect(account: int, password: str, isp: str):
     """Config your GUET-WIFI account and password"""
-    config = {"account": account, "password": password, "isp": isp}
-    config = {k: v for k, v in config.items() if v is not None}
-    config_path = os.path.join(_path, "config.json")
-    if os.path.exists(config_path):
-        with open(config_path, "r") as f:
-            config.update(json.load(f))
-    with open(config_path, "w") as f:
-        json.dump(config, f)
+    if NetWork.checkNetwork():
+        print("You have already connected to the network")
+        return
+    net = NetWork(account=account, password=password, isp=isp)
+    net.login()
 
 
 def main():
--- guetwifi/guetwifirunner.py
+++ guetwifi/guetwifirunner.py
@@ -1,105 +1,50 @@
-#!/usr/bin/env python
-# coding:utf-8
-
 import requests
 import re
-import time
-import logging
-import os
 import json
-import subprocess
-import click
 import base64
-
-_path = os.path.dirname(os.path.abspath(__file__))
-
-
-class NetWorkConnectLog:
-    def __init__(self, log_file) -> None:
-        self.log_file = os.path.join(_path, log_file)
-        self.logger = logging.getLogger("NetWorkConnectLog")
-        self.logger.setLevel(logging.DEBUG)
-        self.formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-        self._set_file_handler()
-
-    def _set_file_handler(self):
-        file_handler = logging.FileHandler(self.log_file)
-        file_handler.setLevel(logging.INFO)
-        file_handler.setFormatter(self.formatter)
-        self.logger.addHandler(file_handler)
 
 
 class NetWork:
-    def __init__(self) -> None:
-        self._loadConfig()
-        self.logger = NetWorkConnectLog("network.log").logger
-        self.logger.info(
-            f"Load config: account:{self.account},isp:{self.isp},password:{self.password}"
-        )
+    def __init__(self, **kwargs) -> None:
         self.wlan_user_ip = ""
         self.wlan_ac_ip = ""
         self.wlan_user_mac = ""
         self.base_url = "http://10.0.1.5:801/eportal/portal/login"
         self.session = requests.Session()
-
-    def _loadConfig(self):
-        config_path = os.path.join(_path, "config.json")
-        if os.path.exists(config_path):
-            with open(config_path, "r") as f:
-                config = json.load(f)
-                try:
-                    self.account = config["account"]
-                    self.password = config["password"]
-                    # base64 encode
-                    self.password = base64.b64encode(self.password.encode()).decode()
-                    self.isp = config["isp"]
-                except KeyError:
-                    click.echo("Please config your account and password")
+        self.account = kwargs.get("account")
+        self.password = kwargs.get("password")
+        self.isp = kwargs.get("isp")
 
     def _getParams(self):
-        URL = "http://www.msftconnecttest.com/connecttest.txt"
-        HEADERS = {
-            "Connection": "Close",
-            "User-Agent": "Microsoft NCSI",
-            "Host": "www.msftconnecttest.com",
-        }
-        res = self.session.get(URL, headers=HEADERS, allow_redirects=False)
-        html = res.text
+        URL = "http://1.2.3.4"
+        res = self.session.get(URL)
+        html = res.url
         pattern = r"wlanuserip=([\d\.]+).*?wlanacip=([\d\.]+).*?wlanusermac=([\w\-]+)"
         m = re.search(pattern, html)
         if m:
             self.wlan_user_ip = m.group(1)
             self.wlan_ac_ip = m.group(2)
             self.wlan_user_mac = m.group(3)
-            self.logger.info(
-                f"Get params: wlan_user_ip:{self.wlan_user_ip},wlan_ac_ip:{self.wlan_ac_ip},wlan_user_mac:{self.wlan_user_mac}"
-            )
-        else:
-            self.logger.error("Get params failed")
-            self.logger.error(f"Response:{html}")
 
     def login(self):
-        self.logger.info(f"Login with account:{self.account}")
         self._getParams()
         pararms = {
             "callback": "dr1003",
             "login_method": "1",
             "user_account": f",0,{self.account}@{self.isp}",
-            "user_password": self.password,
+            "user_password": base64.b64encode(self.password.encode()).decode(),
             "wlan_user_ip": self.wlan_user_ip,
             "wlan_user_ipv6": "",
             "wlan_user_mac": self.wlan_user_mac,
             "wlan_ac_ip": self.wlan_ac_ip,
             "wlan_ac_name": "HJ-BRAS-ME60-01",
         }
-        self.logger.debug(f"Login params:{pararms}")
         res = self.session.get(self.base_url, params=pararms)
-        self.logger.debug(f"Login response:{res.text}")
         if self.checkResult(res.text):
-            self.logger.info("Login success")
+            print("Login success")
         else:
-            self.logger.error("Login failed")
-            self.logger.error(f"Response:{res.text}")
+            print("Login failed")
+            print(f"Error message: {res.text}")
 
     def checkResult(self, html):
         match = re.search(r"{.*}", html)
@@ -114,26 +59,12 @@ class NetWork:
                 return False
 
     @staticmethod
-    def checkNet():
-        # ping www.baidu.com
-        command = "ping -c 5 www.baidu.com"
-        res = subprocess.Popen(
-            command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
-        res.wait()
-        if res.returncode == 0:
+    def checkNetwork():
+        import urllib3
+
+        try:
+            http = urllib3.PoolManager()
+            http.request("GET", "https://baidu.com")
             return True
-        else:
+        except:
             return False
-
-    def run(self):
-        while True:
-            if not self.checkNet():
-                self.logger.info("Network is not connected, start login......")
-                self.login()
-            time.sleep(5)
-
-
-if __name__ == "__main__":
-    network = NetWork()
-    network.run()
